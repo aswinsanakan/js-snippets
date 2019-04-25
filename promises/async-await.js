a4ea5c3c973525531cb92272d78847162ae1d316
Aswin Sanakan <aswinsanakan@gmail.com>
2019-04-25 13:11:13 +0530

    Create promises/async-await.js
    Create promises snippets

---
+++ promises/async-await.js
@@ -0,0 +1,15 @@
+/*
+Error Handling on await
+*/
+
+// Create a wrapper that returns [err,data]
+export default function to(promise) {
+   return promise.then(data => {
+      return [null, data];
+   })
+   .catch(err => [err]);
+}
+
+// Import the wrapper and use it on `await` 
+[err, user] = await to(UserModel.findById(1));
+
